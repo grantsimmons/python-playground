78911d1c6f857c5e1ae0bf85af5ad8215179cd94
Grant Simmons <gsimm706@gmail.com>
2019-08-13 14:07:37 -0400

    Reorg/Optimize

--- webscrape-testing/webscrape.py
+++ webscrape-testing/webscrape.py
@@ -3,22 +3,19 @@ from urllib.request import urlopen as uReq
 my_url = 'https://u.demog.berkeley.edu/~andrew/1918/figure2.html'
 #my_url = 'https://en.wikipedia.org/wiki/Special:AllPages'
 uClient = uReq(my_url)
-page_html = uClient.read()
+#page_html = uClient.read()
+page_soup = soup(uClient.read(), "html.parser")
 uClient.close()
-page_soup = soup(page_html, "html.parser")
 rows = page_soup.findAll("tr")
 filename = "lifespans.csv"
 f = open(filename, "w")
 
-for i, row in enumerate(rows):
-    if i < 3:
-        continue
+for row in rows[3:len(rows) - 1]:
     current = row.findAll("p")
     for index, item in enumerate(current):
         if index != 2:
             f.write(item.text + ",")
         else:
-            f.write(item.text)
-    f.write("\n")
+            f.write(item.text + "\n")
 
 f.close()
