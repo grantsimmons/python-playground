c33972549c8e669b9f701baea3ce50c156c397dd
Grant Simmons <gsimm706@gmail.com>
2020-02-17 10:38:18 -0500

    Trace Data

--- webscrape-testing/tester.sh
+++ webscrape-testing/tester.sh
@@ -3,9 +3,10 @@ do
     echo $1
     echo "Tracing $1, round $i"
     date "+%Y-%m-%d %H:%M:%S" >> trace_$1.txt
-    inetutils-traceroute --resolve-hostnames -M icmp $1 >> trace_$1.txt
+    inetutils-traceroute --resolve-hostnames $1 >> trace_$1.txt
     echo "Tracing $2, round $i"
     date "+%Y-%m-%d %H:%M:%S" >> trace_$2.txt
-    inetutils-traceroute --resolve-hostnames -M icmp $2 >> trace_$2.txt
-    sleep(250)
+    inetutils-traceroute --resolve-hostnames $2 >> trace_$2.txt
+    echo "Sleeping"
+    sleep 250
 done
